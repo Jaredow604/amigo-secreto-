999fd068ad7fde8901b4f142f4e9a4ccfc41434d
Jaredow604 <143060313+Jaredow604@users.noreply.github.com>
2025-08-19 17:24:27 -0600

    Agregue funcion añadir amigos, limpiar caja, checar correcto funcionamieto al trabajar en maquina principal

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 let listAmigos[];
+function añadirAmigo(){
+    let amigos=0;
+    let nuevoAmigo=document.getElementById('amigo');
+    listAmigos[amigos].push(nuevoAmigo);
+    amigos ++;
+}
+function limpiarCaja(){
+    document.querySelector('#Valor usuario').value='';
+}
